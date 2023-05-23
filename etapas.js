491e27d1d8b84d26664c9cf1825815c35c69d3e6
Victor Ian <107140045+kbldevelop@users.noreply.github.com>
2023-05-22 22:26:30 -0300

    Terminado

---
+++ etapas.js
@@ -0,0 +1,44 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'Jair Bolsonaro',
+                partido: 'PL',
+                fotos: [
+                    {url: "bolsonaro.jpeg", legenda: 'Presidente', small: false},
+                    {url: "braga.jpeg", legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Luís Inacio Lula da Silva',
+                partido: 'PT',
+                fotos: [
+                    {url: 'lula.jpg', legenda: 'Presidente'},
+                    {url: 'alkmin.jpg', legenda: 'Vice-Presidente'}
+                ]
+            },
+            {
+                numero: '12',
+                nome: 'Ciro Gomes',
+                partido: 'PDT',
+                fotos: [
+                    {url: 'ciro gomes.jpg', legenda: 'Presidente'},
+                    
+                ]
+            },
+            {
+                numero: '15',
+                nome: 'Simone Tabet',
+                partido: 'MDB',
+                fotos: [
+                    {url: 'simone.jpg', legenda: 'Presidente'}
+
+                ]
+            },
+        ]
+    },
+]
